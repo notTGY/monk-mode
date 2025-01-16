e92a18f4b45ba1ba8acaabe85ae857b2ecb7d87e
notTGY <microcraft47@gmail.com>
2025-01-16 10:35:10 +0300

    mvp

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # monk-mode
 Disable distractions on the web
+
+1. static rule for `declarative_net_request`
+to redirect all images to my website as proxy.
+Pitfall: problems if browser is fingerprinted.
+```
+{
+  "id": 1,
+  "priority": 1,
+  "action": {
+    "type": "redirect",
+    "redirect": {
+      "regexSubstitution": "https://\\1.xyz.com/"
+    }
+  },
+  "condition": {
+    "regexFilter": "^https://www\\.(abc|def)\\.xyz\\.com/",
+    "resourceTypes": [
+      "main_frame"
+    ]
+  }
+}
+```
+
+2. PWA-like background service worker.
+```
+{
+  "name": "Awesome Test Extension",
+  ...
+  "background": {
+    "service_worker": "service-worker.js",
+    "type": "module"
+  },
+  ...
+}
+```
+Pitfalls: 30 seconds of no events -> shuts down sw,
+single request takes more than 5 minutes,
+when fetch() takes more than 30 seconds.
+```
+self.addEventListener('fetch', (event) => {
+  if (event.request.destination === 'image') {
+    event.respondWith(/* your caching logic here */);
+  }
+
+  // If you don't call event.respondWith() for some requests,
+  // the normal loading behavior will be used by default.
+};
+```
+
+3. Manually pixelate images inside content script
---
+++ extension/index.js
@@ -0,0 +1,68 @@
+const c = document.createElement('canvas')
+const ctx = c.getContext('2d', { willReadFrequently: true })
+
+const SIZE = 15
+
+const pixelate = (image) => {
+  const { width, height } = image.getBoundingClientRect()
+  let w = SIZE
+  let h = SIZE
+  if (width > height) {
+    h = Math.ceil(SIZE*height/width)
+    if (width == 0) {
+      return
+    }
+  } else {
+    w = Math.ceil(SIZE*width/height)
+    if (height == 0) {
+      return
+    }
+  }
+  if (w == 0 || h == 0) {
+    return
+  }
+  c.width = w
+  c.height = h
+
+  ctx.drawImage(image, 0, 0, w, h)
+  /* GRAYSCALE
+  const imageData = ctx.getImageData(0, 0, w, h)
+  const data = imageData.data
+  for (let i = 0; i < data.length / 4; i++) {
+    const r = data[i*4 + 0]
+    const g = data[i*4 + 1]
+    const b = data[i*4 + 2]
+    const o = data[i*4 + 3]
+
+    const sum = r + g + b
+    data[i*4 + 0] = sum/3
+    data[i*4 + 1] = sum/3
+    data[i*4 + 2] = sum/3
+  }
+  ctx.putImageData(imageData, 0, 0)
+  */
+
+  image.src = c.toDataURL()
+}
+
+const addAttributes = () => {
+  const images = document.querySelectorAll('img')
+  for (const image of images) {
+    pixelate(image)
+    image.setAttribute('data-substituted', true)
+  }
+}
+
+
+
+document.addEventListener('scroll', () => {
+  addAttributes()
+})
+document.addEventListener('mousemove', () => {
+  addAttributes()
+})
+document.addEventListener('click', () => {
+  addAttributes()
+})
+addAttributes()
+document.body.onload = addAttributes
