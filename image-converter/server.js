1783a53a28b196c25d8d9b4a1e9fdd24781a2f8a
notTGY <microcraft47@gmail.com>
2025-01-17 09:24:15 +0300

    replace express with hono

--- image-converter/server.js
+++ image-converter/server.js
@@ -1,13 +1,16 @@
-const express = require('express')
-const app = express()
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
+
+const app = new Hono()
+app.use('/*', serveStatic({ root: './dist', }))
+
+app.get('/api/email', (c) => {
+  console.log(c.req.query)
+  c.text('ok')
+})
+
 const port = process.env.PORT || 3000
+const server = serve({ fetch: app.fetch, port })
 
-app.use(express.static('./dist'))
-app.get('/api/email', (req, res) => {
-  console.log(req.query)
-  res.send('ok')
-})
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+console.log(`Server is running on port ${port}`)
