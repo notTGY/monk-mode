e621ea18a5e58d27d7db4893eae98f3326bba0fd
notTGY <microcraft47@gmail.com>
2025-01-21 14:46:26 +0300

    remove redudant theme syncronization

--- popup-vite-app/src/components/theme-provider.tsx
+++ popup-vite-app/src/components/theme-provider.tsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useEffect, useState } from "react"
-import { storage } from '@/lib/storage'
 
 type Theme = "dark" | "light" | "system"
 
@@ -19,12 +18,6 @@ const initialState: ThemeProviderState = {
   setTheme: () => null,
 }
 
-const fetchTheme = async (): Promise<Theme> => {
-  const obj: any = await storage.get('theme')
-  const theme = obj.theme as Theme
-  return theme
-}
-
 const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 
 export function ThemeProvider({
@@ -36,14 +29,6 @@ export function ThemeProvider({
   const [theme, setTheme] = useState<Theme>(
     () => (localStorage.getItem(storageKey) as Theme) || defaultTheme
   )
-
-  useEffect(() => {
-    fetchTheme().then(theme => {
-      if (theme) {
-        setTheme(theme)
-      }
-    })
-  }, [])
 
   useEffect(() => {
     const root = window.document.documentElement
