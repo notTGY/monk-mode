01e98d7bbd1445d97e4883f6ad64d6d641fd9ce6
notTGY <microcraft47@gmail.com>
2025-01-23 01:21:20 +0300

    changelog start

--- pixelify-landing/astro.config.mjs
+++ pixelify-landing/astro.config.mjs
@@ -5,7 +5,9 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), mdx()]
 });
---
+++ pixelify-landing/src/content.config.ts
@@ -0,0 +1,24 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const changelog = defineCollection({
+  loader: glob({
+    base: './src/content/changelog',
+    pattern: '**/*.{md,mdx}'
+  }),
+  schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { changelog };
