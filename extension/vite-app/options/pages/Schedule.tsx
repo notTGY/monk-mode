3507b9a9acb88aabc64117ecf48cb0eed40e238d
notTGY <microcraft47@gmail.com>
2025-01-26 20:12:34 +0300

    calendar start

--- extension/vite-app/options/pages/Schedule.tsx
+++ extension/vite-app/options/pages/Schedule.tsx
@@ -1,5 +1,44 @@
+import { useRef, useEffect } from 'react'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+
+
+function Calendar() {
+  const PanelGroup = useRef(null)
+  const onLayout = (sizes: number[]) => {
+    //console.log({sizes})
+  }
+
+  useEffect(() => {
+    const pg = PanelGroup.current
+    if (pg != null) {
+      console.log(pg.getLayout())
+    }
+  }, [PanelGroup.current])
+
+  return (
+    <div className="h-[300px]">
+      <ResizablePanelGroup
+        direction="vertical"
+        onLayout={onLayout}
+        ref={PanelGroup}
+      >
+        <ResizablePanel>One</ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel>Two</ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  )
+}
+
 export default function Schedule() {
   return (
-    <div>hi</div>
+    <main>
+      hi
+      <Calendar/>
+    </main>
   )
 }
